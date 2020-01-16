2cadf76b214bc2a37828f9ffe96325f854f97aa4
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2020-01-16 13:03:52 +0800

    make card clickable

---
+++ app/src/main/java/com/example/finddine/ClientInterface/Activities/NavigationStart.kt
@@ -0,0 +1,13 @@
+package com.example.finddine.ClientInterface.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.finddine.R
+
+class NavigationStart : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation_start)
+    }
+}
--- app/src/main/java/com/example/finddine/ClientInterface/StallRecycler/StallAdaptor.kt
+++ app/src/main/java/com/example/finddine/ClientInterface/StallRecycler/StallAdaptor.kt
@@ -1,13 +1,18 @@
 package com.example.finddine.ClientInterface.StallRecycler
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
-import com.example.finddine.R
+import com.example.finddine.ClientInterface.Activities.NavigationStart
 import com.example.finddine.R.layout.stall_list_item
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.stall_list_item.view.*
+
 
 class StallAdaptor (val items : ArrayList<Stall>, val context: Context) : RecyclerView.Adapter<StallAdaptor.ViewHolder>() {
 
@@ -16,9 +21,11 @@ class StallAdaptor (val items : ArrayList<Stall>, val context: Context) : Recycl
     }
 
     class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
+
         val stallName = view.stall_name
         val foodName = view.food_name
         val stallRating = view.stall_rating
+        val stallCard = view.stall_card
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -27,10 +34,19 @@ class StallAdaptor (val items : ArrayList<Stall>, val context: Context) : Recycl
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = items[position]
-        holder?.stallName?.text = item.name
+        val itemNum = position + 1
+        holder?.stallName?.text = itemNum.toString() + ". " + item.name
         holder?.foodName?.text = item.food
         holder?.stallRating?.text = item.rating.toString() + "\u2605"
         holder?.stallRating?.contentDescription = item.rating.toString() + "stars"
+
+        holder?.stallCard.isClickable = true
+
+        holder?.stallCard.setOnClickListener{ v:View ->
+            val intent = Intent(context, NavigationStart::class.java)
+            context.startActivity(intent);
+        }
+
     }
 
 }
