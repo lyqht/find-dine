32f31f43a0435b3c0faf1c97464d709fb3a4a591
lyqht <estee.tsw@gmail.com>
2020-01-16 17:07:53 +0800

    Navigation Start Screen add Refresh Button and mock instructions

--- app/src/main/java/com/example/finddine/ClientInterface/Activities/NavigationStart.kt
+++ app/src/main/java/com/example/finddine/ClientInterface/Activities/NavigationStart.kt
@@ -8,11 +8,13 @@ import androidx.databinding.DataBindingUtil
 import com.example.finddine.R
 import com.example.finddine.databinding.ActivityNavigationStartBinding
 import java.util.*
+import kotlin.collections.ArrayList
 
 class NavigationStart : AppCompatActivity() {
 
     private lateinit var binding: ActivityNavigationStartBinding
-    private lateinit var tts: TextToSpeech
+    private var counter: Int = 0
+    private var instructions: ArrayList<String> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +22,28 @@ class NavigationStart : AppCompatActivity() {
         val extra: Bundle? = getIntent().getExtras()
         val stall_name = extra?.getString("name")
         binding.directionsText.setText("Directions to " + stall_name)
+        setInstructions()
+
         binding.navigationExitText.setOnClickListener { finish() }
+
+        binding.refreshButton.setOnClickListener {
+            counter += 1
+            val currentInstruction = getInstruction()
+            binding.instructionText.text = currentInstruction
+            binding.refreshButton.contentDescription = currentInstruction + ". Double Tap to refresh instruction"
+        }
+    }
+
+    fun setInstructions() {
+        instructions.add("Walk 40m forwards, to the next turn")
+        instructions.add("Turn left to your 9 o'clock.")
+        instructions.add("Walk 5m forward to stall Wang Jia Ban Mian")
+        instructions.add("Turn left")
+        instructions.add("You have reached stall Wang Jia Ban Mian")
+    }
+
+    fun getInstruction(): String {
+        return instructions.get(counter)
     }
 
 }
